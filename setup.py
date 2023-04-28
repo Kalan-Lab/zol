55fd5b9908b449585bfd6009a2b7a777ed1843e1
Rauf Salamzade <salamzader@gmail.com>
2023-04-27 22:15:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 
 setup(name='zol',
-      version='1.0.2.1',
+      version='1.0.2',
       description='',
       url='http://github.com/Kalan-Lab/zol/',
       author='Rauf Salamzade',

50d47abe8d7b26d39fba30553686c8bf07e2b41b
Rauf Salamzade <salamzader@gmail.com>
2024-10-29 11:10:13 -0500

    update test_docker.sh

--- docker/test_docker.sh
+++ docker/test_docker.sh
@@ -22,17 +22,17 @@ ln -s ../run_ZOL.sh .
 ./run_ZOL.sh prepTG -i Target_Genomes/ -o prepTG_Database/ -c 4
 
 # Step 2a: run fai (input type 1) to identify orthologous instances of epa from E. faecalis
-./run_ZOL.sh fai -i Epa_MIBiG_GBK/ -tg prepTG_Database/ -o fai_Results_1/ -c 4 --generate_plots
+./run_ZOL.sh fai -i Epa_MIBiG_GBK/ -tg prepTG_Database/ -o fai_Results_1/ -c 4 --generate-plots
 
 # Step 2b: run fai (input type 2) to identify orthologous instances of epa from E. faecalis
 ./run_ZOL.sh fai -r Efaecalis_V583_Genome.fasta -rc NC_004668.1 -rs 2083902 -re 2115174 -tg prepTG_Database/ \
-           -o fai_Results_2/ -c 4 --generate_plots
+           -o fai_Results_2/ -c 4 --generate-plots
 
 # Step 2c: run fai (input type 3) to identify orthologous instances of epa from E. faecalis
-./run_ZOL.sh fai -pq Epa_Proteins_from_MIBiG_GenBank.faa -tg prepTG_Database/ -o fai_Results_3/ -c 4 --generate_plots
+./run_ZOL.sh fai -pq Epa_Proteins_from_MIBiG_GenBank.faa -tg prepTG_Database/ -o fai_Results_3/ -c 4 --generate-plots
 
 # Step 3: run zol to perform comparative investigations of gene-clusters
-./run_ZOL.sh zol -i fai_Results_2/Final_Results/Homologous_Gene_Cluster_GenBanks/ -o zol_Results/ -c 4 --full_genbank_labels
+./run_ZOL.sh zol -i fai_Results_2/Final_Results/Homologous_Gene_Cluster_GenBanks/ -o zol_Results/ -c 4 --full-genbank-labels
 
 # Step 4: run cgc / cgcg for collapsed gene cluster visualization generation
 ./run_ZOL.sh cgc -i zol_Results/ -o cgc_Results/

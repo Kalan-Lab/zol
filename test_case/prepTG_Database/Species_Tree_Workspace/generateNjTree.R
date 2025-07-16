4ca92369133bb687a42f52f426f934bec10ceb1a
Rauf Salamzade <salamzader@gmail.com>
2025-07-16 11:28:08 -0700

    update to v1.6.0 - fix handling of clustered prots

--- test_case/prepTG_Database/Species_Tree_Workspace/generateNjTree.R
+++
@@ -1,9 +0,0 @@
-library(ape)
-
-input.dist.file <- "/Users/raufs/Coding/zol/develop_v1.6.0/commit/zol/test_case/prepTG_Database/Species_Tree_Workspace/Skani_Based_Distance_Matrix.txt"
-output.nwk.file <- "/Users/raufs/Coding/zol/develop_v1.6.0/commit/zol/test_case/prepTG_Database/Species_Tree_Workspace/Unrooted_Species_Tree.nwk"
-
-dat <- read.table(input.dist.file, header=T, sep="\t", row.names=1)
-d <- as.dist(as.matrix(dat))
-njt <- nj(d)
-write.tree(njt, file=output.nwk.file)
